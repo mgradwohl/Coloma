0665c849bbb84284b845ace0f12dfc52a7da8cc2
jberenhaus <jberenhaus@gmail.com>
2016-06-21 17:46:02 -0700

    Moving some code into functions

--- ColomaAnalysis/Program.cs
+++ ColomaAnalysis/Program.cs
@@ -15,8 +15,7 @@ namespace ColomaAnalysis
             var files = Directory.EnumerateFiles(folderPath, "*.tsv").ToArray();
             foreach (string file in files)
             {
-                Console.ForegroundColor = ConsoleColor.DarkGray;
-                Console.WriteLine($" Reading {file}");
+                ReadFileLogToConsole(file);
                 string[] lines;
                 try
                 {
@@ -29,17 +28,7 @@ namespace ColomaAnalysis
                     filesFailed++;
                     continue;
                 }
-                Console.Write("[");
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.Write($"{filesSucceeded} ");
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.Write("|");
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write($" {filesFailed}");
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine($"] Writing {file}");
-                Console.ForegroundColor = ConsoleColor.DarkCyan;
-                Console.WriteLine($"Files remaining: {files.Count() - (filesSucceeded + filesFailed)}");
+                WrittenFileLogToConsole(filesSucceeded, filesFailed, file, files.Count());
                 //TODO: Add to a buffer first and then write to the file for faster run time
                 //TODO: Delete the old file first, this will just append to the file forever if you don't delete it
                 //TODO: Maybe add a timestamp?
@@ -56,6 +45,27 @@ namespace ColomaAnalysis
             Console.WriteLine($"Files Failed: {filesFailed}");
             Console.WriteLine("Done, thank you. Hit any key to exit");
             Console.ReadKey(true);
+        }
+
+        private static void ReadFileLogToConsole(string file)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine($"Reading {file}");
+        }
+
+        private static void WrittenFileLogToConsole(int filesSucceeded, int filesFailed, string file, int fileCount)
+        {
+            Console.Write("[");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write($"{filesSucceeded} ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("|");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write($" {filesFailed}");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"] Writing {file}");
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine($"Files remaining: {fileCount - (filesSucceeded + filesFailed)}");
         }
     }
 }
